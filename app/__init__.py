d87a4f339a7b91b6078f5cd1f9bae17dbae5e845
Roy-Lam <166689528+Roy-Lam@users.noreply.github.com>
2024-05-10 14:51:11 +0800

    imported flask-login with secret key and space for sqlalchemy URI

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,25 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
 from app import routes, models
-from .routes import app
+
+# This is used to load a user from the user_id stored in the session
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(int(user_id))
+
+class Config:
+    SECRET_KEY = 'anotsecureenoughkeymayneedchanges'
+    SQLALCHEMY_DATABASE_URI = 'database_uri'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
