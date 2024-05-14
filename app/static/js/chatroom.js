0195a2d569383f2bb478caae8c5d52e1cdeabfa4
CelynChew <celynchew1703@gmail.com>
2024-05-14 17:36:02 +0800

    cleaned up mobile chatroom view (#10)

--- app/static/js/chatroom.js
+++ app/static/js/chatroom.js
@@ -413,22 +413,11 @@ function switchContent() {
 // Listener to switch content on screen resizing
 window.addEventListener('resize', switchContent);
 
-// Listener to show chat list and search bar in mobile view
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the sidebar element
-    var sidebar = document.getElementById('sidebar');
-    // Get the toggle sidebar button
-    var toggleSidebarBtn = document.getElementById('toggle-sidebar-btn');
-    // Get the search chat input
-    var searchChatInput = document.getElementById('search-chat');
-    // Get the chat list
-    var chatList = document.getElementById('chat-list');
+// Functions to open and close sidenav bar
+function openNav() {
+    document.getElementById("chats").style.width = "250px";
+}
 
-    // Add click event listener to the toggle sidebar button
-    toggleSidebarBtn.addEventListener('click', function() {
-        // Toggle the visibility of the search chat input
-        searchChatInput.style.display = searchChatInput.style.display === 'none' ? 'block' : 'none';
-        // Toggle the visibility of the chat list
-        chatList.style.display = chatList.style.display === 'none' ? 'block' : 'none';
-    });
-});
+function closeNav() {
+    document.getElementById("chats").style.width = "0";
+}
